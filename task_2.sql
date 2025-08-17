1237d87d902da4af340370ae1b01cee0c1b2b18a
mhabtom <meski123habtom@gmail.com>
2025-08-17 21:32:46 +0200

    Add task_2.sql: create bookstore tables

---
+++ task_2.sql
@@ -0,0 +1,57 @@
+-- USE TARGET DATABASE
+USE alx_book_store;
+
+-- AUTHORS
+CREATE TABLE IF NOT EXISTS Authors (
+  author_id INT NOT NULL AUTO_INCREMENT,
+  author_name VARCHAR(215) NOT NULL,
+  PRIMARY KEY (author_id)
+) ENGINE=INNODB;
+
+-- BOOKS
+CREATE TABLE IF NOT EXISTS Books (
+  book_id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(130) NOT NULL,
+  author_id INT NOT NULL,
+  price DOUBLE NOT NULL,
+  publication_date DATE,
+  PRIMARY KEY (book_id),
+  CONSTRAINT FK_Books_Author
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+      ON UPDATE RESTRICT ON DELETE RESTRICT
+) ENGINE=INNODB;
+
+-- CUSTOMERS
+CREATE TABLE IF NOT EXISTS Customers (
+  customer_id INT NOT NULL AUTO_INCREMENT,
+  customer_name VARCHAR(215) NOT NULL,
+  email VARCHAR(215),
+  address TEXT,
+  PRIMARY KEY (customer_id)
+) ENGINE=INNODB;
+
+-- ORDERS
+CREATE TABLE IF NOT EXISTS Orders (
+  order_id INT NOT NULL AUTO_INCREMENT,
+  customer_id INT NOT NULL,
+  order_date DATE NOT NULL,
+  PRIMARY KEY (order_id),
+  CONSTRAINT FK_Orders_Customer
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+      ON UPDATE RESTRICT ON DELETE RESTRICT
+) ENGINE=INNODB;
+
+-- ORDER_DETAILS
+CREATE TABLE IF NOT EXISTS Order_Details (
+  orderdetailid INT NOT NULL AUTO_INCREMENT,
+  order_id INT NOT NULL,
+  book_id INT NOT NULL,
+  quantity DOUBLE NOT NULL,
+  PRIMARY KEY (orderdetailid),
+  CONSTRAINT FK_OrderDetails_Order
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+      ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT FK_OrderDetails_Book
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+      ON UPDATE RESTRICT ON DELETE RESTRICT
+) ENGINE=INNODB;
